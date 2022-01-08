5413f75b7a31c3b084fafa4f764a1011540682e2
mgr <koumgrars13@gmail.com>
2022-01-08 16:02:29 +0900

    until Assets, Metadata, and CSS

---
+++ pages/posts/first-post.tsx
@@ -0,0 +1,28 @@
+import Link from 'next/link'
+import Head from 'next/head'
+import Image from 'next/image'
+import Layout from '../../components/layout'
+
+export default function FirstPost() {
+  return (
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">
+          <a>Back To Home.</a>
+        </Link>
+      </h2>
+      <div>
+        <Image
+         src="/images/profile.jpg"
+         width={144}
+         height={144}
+         alt="Your Profile"
+        />
+      </div>
+    </Layout>
+  )
+}
